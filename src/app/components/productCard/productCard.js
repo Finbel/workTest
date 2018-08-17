dcc935b9690d3b16ce59fe158b587681d1e54da6
finbel <p.oregan.87@gmail.com>
2018-08-17 17:22:09 +0200

    tweak font sizes and handle too long names in product card

--- src/app/components/productCard/productCard.js
+++ src/app/components/productCard/productCard.js
@@ -7,17 +7,28 @@ class ProductCard extends Component {
         return Math.round(parseInt(x,10)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     }
 
+    makeEllipsis(s) {
+        if (s.length > 36) {
+            const visibleName = s.substring(0,37);
+            const lastVisibleWord = visibleName.lastIndexOf(" "); 
+            return visibleName.substring(0,lastVisibleWord) + " ...";
+        } else {
+            return s;
+        }
+    }
+
     render() {
         const { name, info, retailerCount, minPrice, stars} = this.props;
         return (
             <div className="product-card">
                 <img className='product-image' src="http://placekitten.com/60/50"/>
                 <div className='product-stars'>{stars}</div>
-                <div className='product-name'>{name}</div>
+                <div className='product-name'>{this.makeEllipsis(name)}</div>
                 <div className='consumer-info'>
-                    fr. 
-                    <span className='amount'>
-                        {this.numberWithSpaces(minPrice)} kr
+                    <span>fr. 
+                        <span className='amount'>
+                            {this.numberWithSpaces(minPrice) + " kr"}
+                        </span>
                     </span>
                     <span className='retailer-count'>
                         {retailerCount} butiker
