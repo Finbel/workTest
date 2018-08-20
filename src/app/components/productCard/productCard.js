839c8837624e2854fe2dcdd024210c34d8b41d1f
finbel <p.oregan.87@gmail.com>
2018-08-20 07:34:56 +0200

    make productCard into a functional component

--- src/app/components/productCard/productCard.js
+++ src/app/components/productCard/productCard.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './productCard.scss';
 import ProductImage from '../productImage/productImage';
 import ProductStars from '../productStars/productStars';
 import ProductInfo from '../productInfo/productInfo';
 import CompareButton from '../compareButton/compareButton';
 
-class ProductCard extends Component {
+function ProductCard(props) {
 
-    render() {
+    const { name,
+            imageURL,
+            info,
+            retailerCount,
+            minPrice,
+            stars,
+            link
+        } = props;
 
-        const { name,
-                imageURL,
-                info,
-                retailerCount,
-                minPrice,
-                stars,
-                link
-            } = this.props;
-
-        return (
-            <div className="product-card">
-                <div className='image-rating-container'>
-                    <ProductImage url={imageURL} />
-                    <ProductStars stars={stars} />
-                </div>
-                <ProductInfo
-                    name={name}
-                    info={info}
-                    minPrice={minPrice}
-                    retailerCount={retailerCount} />
-                <div className="button-container">
-                    <CompareButton link={link} />
-                </div>
+    return (
+        <div className="product-card">
+            <div className='image-rating-container'>
+                <ProductImage url={imageURL} />
+                <ProductStars stars={stars} />
             </div>
-        );
-    }
+            <ProductInfo
+                name={name}
+                info={info}
+                minPrice={minPrice}
+                retailerCount={retailerCount} />
+            <div className="button-container">
+                <CompareButton link={link} />
+            </div>
+        </div>
+    );
 }
 
 export default ProductCard;
