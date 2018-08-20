caf21e7cceea16ddd13aea0715de0a8a11947353
finbel <p.oregan.87@gmail.com>
2018-08-20 07:22:46 +0200

    make productStars into a functional component

--- src/app/components/productStars/productStars.js
+++ src/app/components/productStars/productStars.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './productStars.scss';
 
-class ProductStars extends Component {
+function generateStars(stars) {
+    const starSource = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Star_icon_stylized.svg';
+    const numStars = Math.round(parseFloat(stars,10));
+    let starList = [];
+    for (var i = 1; i <= numStars; i++) {
+        starList.push(<img src={starSource} key={i} height='10' width='10' />);
+     }
+    return starList;
+}
 
-    generateStars(stars) {
-        const starSource = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Star_icon_stylized.svg';
-        const numStars = Math.round(parseFloat(stars,10));
-        let starList = [];
-        for (var i = 1; i <= numStars; i++) {
-            starList.push(<img src={starSource} key={i} height='10' width='10' />);
-         }
-        return starList;
-    }
-
-    render() {
-        return (
-            <div className='product-stars'>
-                {this.generateStars(this.props.stars)}
-            </div>
-        );
-    }
-
+function ProductStars() {
+    return (
+        <div className='product-stars'>
+            {this.generateStars(this.props.stars)}
+        </div>
+    );
 }
 
 export default ProductStars;
