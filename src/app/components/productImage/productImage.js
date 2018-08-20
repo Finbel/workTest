7db611aa3dd41e3056ef02216990226335410442
finbel <p.oregan.87@gmail.com>
2018-08-20 07:21:25 +0200

    make productInfo into a functional component

--- src/app/components/productImage/productImage.js
+++ src/app/components/productImage/productImage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './productImage.scss';
 
 function fallbackSrc(ev) {
--- src/app/components/productInfo/productInfo.js
+++ src/app/components/productInfo/productInfo.js
@@ -1,48 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './productInfo.scss';
 
-class ProductInfo extends Component {
-    numberWithSpaces(price) {
-        return Math.round(parseInt(price,10)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+function numberWithSpaces(price) {
+    return Math.round(parseInt(price,10)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+function makeEllipsis(name) {
+    const limit = 50;
+    if (name.length > limit) {
+        const visibleName = name.substring(0,limit+1);
+        const lastVisibleWord = visibleName.lastIndexOf(' ');
+        return visibleName.substring(0,lastVisibleWord) + ' ...';
+    } else {
+        return name;
     }
+}
 
-    makeEllipsis(name) {
-        const limit = 50;
-        if (name.length > limit) {
-            const visibleName = name.substring(0,limit+1);
-            const lastVisibleWord = visibleName.lastIndexOf(' ');
-            return visibleName.substring(0,lastVisibleWord) + ' ...';
-        } else {
-            return name;
-        }
-    }
-
-    render() {
-
-        const { name,
-            info,
-            retailerCount,
-            minPrice,
-        } = this.props;
-
-        return (
-            <div className='info-container'>
-                <div className='product-name'>{this.makeEllipsis(name)}</div>
-                <div className='product-info'>{info}</div>
-                <div className='consumer-info'>
-                    <span>fr.
-                        <span className='amount'>
-                            {this.numberWithSpaces(minPrice) + ' kr'}
-                        </span>
+function ProductInfo(props) {
+    return (
+        <div className='info-container'>
+            <div className='product-name'>{makeEllipsis(props.name)}</div>
+            <div className='product-info'>{props.info}</div>
+            <div className='consumer-info'>
+                <span>fr.
+                    <span className='amount'>
+                        {numberWithSpaces(props.minPrice) + ' kr'}
                     </span>
-                    <span className='retailer-count'>
-                        {retailerCount} butiker
-                    </span>
-                </div>
+                </span>
+                <span className='retailer-count'>
+                    {props.retailerCount} butiker
+                </span>
             </div>
-        );
-    }
-
+        </div>
+    );
 }
 
 export default ProductInfo;
