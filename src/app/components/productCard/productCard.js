1544f1bb9328511434fab504eaf0230582f46a8c
finbel <p.oregan.87@gmail.com>
2018-08-20 10:12:34 +0200

    render product card conditionally as a or div depending on window width

--- src/app/components/productCard/productCard.js
+++ src/app/components/productCard/productCard.js
@@ -5,9 +5,47 @@ import ProductStars from '../productStars/productStars';
 import ProductInfo from '../productInfo/productInfo';
 import CompareButton from '../compareButton/compareButton';
 
+/*
+    A wrapper for productCard that handles whether the card should be a link in itself or not.
+*/
+function ProductCardWrapper(props) {
+    let wrapper = null;
+    if (props.width<768) {
+        wrapper = <a className='product-card' href={props.link}>{props.children}</a>;
+    } else {
+        wrapper = <div className='product-card' href={props.link}>{props.children}</div>;
+    }
+    return wrapper;
+}
+
 class ProductCard extends Component {
 
+    /*
+        Need state to manage window dimensions in order to decide whether the card should be a link in itself or have the link as a button. The following 18 lines come from:
+        https://stackoverflow.com/a/42141641/4136445
+    */
+    constructor(props) {
+        super(props);
+        this.state = { width: 0};
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth});
+    }
+
     render() {
+
+        const {width} = this.state;
 
         const { name,
                 imageURL,
@@ -18,8 +56,11 @@ class ProductCard extends Component {
                 link
             } = this.props;
 
+        const compareUrl = 'https://pricerunner.se'+link;
+        const compareButton = width<768 ? null : <CompareButton link={compareUrl} />;
+
         return (
-            <div className="product-card">
+            <ProductCardWrapper width={width} link={compareUrl}>
                 <div className='image-rating-container'>
                     <ProductImage url={imageURL} />
                     <ProductStars stars={stars} />
@@ -29,10 +70,10 @@ class ProductCard extends Component {
                     info={info}
                     minPrice={minPrice}
                     retailerCount={retailerCount} />
-                <div className="button-container">
-                    <CompareButton link={link} />
+                <div className='button-container'>
+                    {compareButton}
                 </div>
-            </div>
+            </ProductCardWrapper>
         );
     }
 }
